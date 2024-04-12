0529d65d75ebf1e72419cd5bfd52760990da4901
nk-tran <nktran4@gmail.com>
2024-04-12 15:28:02 -0400

    js

---
+++ wwwroot/js/carRentalSearch.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    $('#searchBtnCar').click(function (event) {
+        event.preventDefault(); 
+
+        var formData = $('#carSearchForm').serialize(); 
+
+        $('#loader').show(); 
+
+        setTimeout(function () {
+            $.ajax({
+                url: $('#carSearchForm').attr('action'), //from _SearchBar.cshtml
+                type: 'POST', 
+                data: formData,
+                success: function (response) {
+                    $('#carList').html(response);  // Car > Index
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                },
+                complete: function () {
+
+                    $('#loader').hide();
+                }
+            });
+        }, 1000); 
+    });
+});
